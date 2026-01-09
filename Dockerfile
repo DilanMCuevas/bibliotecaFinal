ad1b1bc4cac126d5d0b90aa02ab71682f12e4eff
dilanmc <dilanmcuevas@gmail.com>
2026-01-09 13:35:46 -0600

    Versión lista para Render

---
+++ Dockerfile
@@ -0,0 +1,14 @@
+# ===== BUILD =====
+FROM eclipse-temurin:17-jdk AS build
+WORKDIR /app
+COPY . .
+# ESTA LÍNEA ES EL SEGURO DE VIDA EN LINUX/RENDER:
+RUN chmod +x mvnw
+RUN ./mvnw clean package -DskipTests
+
+# ===== RUN =====
+FROM eclipse-temurin:17-jre
+WORKDIR /app
+COPY --from=build /app/target/*.jar app.jar
+EXPOSE 8080
+ENTRYPOINT ["java","-jar","app.jar"]
---
+++ src/main/java/com/ipn/mx/biblioteca/config/OpenApiConfig.java
@@ -0,0 +1,25 @@
+package com.ipn.mx.biblioteca.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.servers.Server;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Biblioteca API",
+                version = "v1",
+                description = "API para la gestión de una biblioteca"
+        ),
+        servers = {
+                @Server(
+                        url = "http://localhost:8080",
+                        description = "Servidor local de desarrollo"
+                )
+        }
+)
+public class OpenApiConfig {
+    // Configuración global de OpenAPI/Swagger
+}
